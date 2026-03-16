f0fb737544c9f339c14795741edf1d1d97a0ef5e
Yisen Cui <yisenschool2016@gmail.com>
2026-03-16 20:22:29 +1100

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,17 @@
+#include <Arduino.h>
+
+// put function declarations here:
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(1, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
+
+// put function definitions here:
+int myFunction(int x, int y) {
+  return x + y;
+}
